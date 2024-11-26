0ed0ab0500208ca9faabb0e8a239f6cdb7ad3a95
IMSEONGBIN506 <bins506@gmail.com>
2024-11-26 10:29:06 +0900

    detail 수정

--- my-app/src/pages/Loading.js
+++ my-app/src/pages/Loading.js
@@ -16,7 +16,7 @@ const Loading = () => {
   return (
     <div className="loading-container">
       <div className="loading-box">
-        <h1 className="loading-text">FOLLOW THE THREAD</h1>
+        <h1 className="loading-text">FOLLOW SCARLET THREADS</h1>
         <p className="sub-text">ESCAPE ROOM TEAM MATCHING</p>
       </div>
     </div>
